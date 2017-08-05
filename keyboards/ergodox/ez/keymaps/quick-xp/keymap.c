3685d406096add2f4590f65b43a7778a6a7a381d
rnishio <ryosuke.nishio@spacemarket.co.jp>
2017-08-05 19:56:16 +0900

    バグ修正

--- keyboards/ergodox/ez/keymaps/quick-xp/keymap.c
+++ keyboards/ergodox/ez/keymaps/quick-xp/keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,    K_EISU,
   KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,    KC_LT,
-  KC_LCTRL,   KC_A        KC_S,       KC_D,       KC_F,       KC_G,
+  KC_LCTRL,   KC_A,        KC_S,       KC_D,       KC_F,       KC_G,
   KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_TAB,
-  KC_CAPS,    KC_ALT,     KC_LGUI,    KC_GRV,     KC_LBRC,
+  KC_CAPS,    KC_LALT,     KC_LGUI,    KC_GRV,     KC_LBRC,
 
               _______,    _______,
                           _______,
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   K_KANA,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINUS,
   KC_GT,      KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
-  CKC_EQL,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SKSH,    S(KC_SCLN),
+  KC_EQL,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    S(KC_SCLN),
                           KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_UP,
 
   _______,    _______,
