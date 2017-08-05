4ce3aa9588ac3222611816103c1dadda5f3a3f91
rnishio <ryosuke.nishio@spacemarket.co.jp>
2017-08-05 21:09:46 +0900

    修正

--- keyboards/ergodox/ez/keymaps/quick-xp/keymap.c
+++ keyboards/ergodox/ez/keymaps/quick-xp/keymap.c
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1! |  2@  |   3# |   4$ |   5% | EISU |           | KANA |  6^  |   7& |   8* |  9(  |  0)  |  BS    |
+ * | ESC    |   1! |  2@  |   3# |   4$ |   5% |  6^  |           |   7& |   8* |  9(  |  0)  |  -_  |  +=  |  BS    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   <  |           |  >   |   Y  |   U  |   I  |   O  |  P   |  |\    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | EISU |           | KANA |   Y  |   U  |   I  |   O  |  P   |  |\    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |  ENTER |
  * |--------+------+------+------+------+------|  "'  |           |  =+  |------+------+------+------+------+--------|
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [L_COMMON] = KEYMAP(  // layer 0 : default
   // left hand
-  KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,    K_EISU,
-  KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,    KC_LT,
-  KC_LCTRL,   KC_A,        KC_S,       KC_D,       KC_F,       KC_G,
+  KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,    KC_6,
+  KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,    K_EISU,
+  KC_LCTRL,   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
   KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,    KC_QUOT,
-  KC_CAPS,    KC_LALT,     KC_LGUI,    KC_GRV,     KC_LBRC,
+  KC_CAPS,    KC_LALT,    KC_LGUI,    KC_GRV,     KC_LBRC,
 
               _______,    _______,
                           _______,
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   //righthand
-  K_KANA,     KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
-  KC_GT,      KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
+     KC_7,    KC_8,       KC_9,       KC_0,       KC_MINUS,   KC_PLUS,    KC_BSPC,
+   K_KANA,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
   KC_EQL,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_MINUS,
                           KC_RBRC,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
